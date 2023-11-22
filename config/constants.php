8e82676fd1f976e5ff713dd6073f1cce68c9f3f2
Otieno <ronvi035@gmail.com>
2023-11-22 12:10:17 +0300

    chore: added config/constants file

--- config/constants.php
+++ config/constants.php
@@ -1,9 +1,12 @@
 <?php 
 
 //create constants to non repeating values
+define('LOCALHOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'food-order');
 
 
-
-$conn = mysqli_connect('localhost', 'root', '') or die(mysqli_error()); //db connection
-$db_select = mysqli_select_db($conn, 'food-order') or die(mysqli_error());//selecting db
+$conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD) or die(mysqli_error()); //db connection
+$db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error());//selecting db
 ?>
