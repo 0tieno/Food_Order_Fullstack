55e7bed011ca7d08d84ee4311d6081db7d973538
Otieno <ronvi035@gmail.com>
2023-11-22 08:18:41 +0300

    initial commit

---
+++ admin/manage-admin.php
@@ -0,0 +1,59 @@
+<?php include('partials/menu.php'); ?>
+
+    <main class="main-content">
+        <div class="wrapper">
+            <h1>Manage Admin</h1>
+            <!-- table -->
+            <br><br>
+            <a href="add-admin.php" class="btn-primary">Add Admin</a>
+            <br><br><br>
+
+            <table class="tbl-full">
+                <tr>
+                    <th>S.N.</th>
+                    <th>Full Name</th>
+                    <th>Username</th>
+                    <th>Actions</th>
+                </tr>
+                <tr>
+                    <td>1.</td>
+                    <td>Nguyen Van A</td>
+                    <td>admin</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2.</td>
+                    <td>Nguyen Van B</td>
+                    <td>admin</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>3.</td>
+                    <td>Nguyen Van C</td>
+                    <td>admin</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+                </tr>
+                <tr>
+                    <td>4.</td>
+                    <td>Nguyen Van D</td>
+                    <td>admin</td>
+                    <td>
+                        <a href="#" class="btn-secondary">Update Admin</a>
+                        <a href="#" class="btn-danger">Delete Admin</a>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </main>
+
+<?php include('partials/footer.php') ?>
+    
---
+++ admin/partials/footer.php
@@ -0,0 +1,9 @@
+    <footer class="footer">
+            <div class="wrapper">
+                <p class="text-center">
+                    @sirronney. All rights reserved! &copy; 2023
+                </p>
+            </div>
+    </footer>
+</body>
+</html>
